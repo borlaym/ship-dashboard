fdd184654c25ecf01a28188f335acbb59671b074
Marton Borlay <borlaym@gmail.com>
2018-02-10 22:57:19 +0100

    feat(PercentageValue): Add tracking bar

--- README.md
+++ README.md
@@ -6,4 +6,4 @@
 
 ## Scenario
 
-You can modify the scenario by editing `scenario.json` and following the pattern already present there. You can add new values simply by adding them to the json, specifying a type, value, label etc. Note that I have no error handling; make sure to add each value to each time stamp. Btw the timestamp is in milliseconds.
+You can modify the scenario by editing `scenario.json` and following the pattern already present there. You can add new values simply by adding them to the json, specifying a type, value, label etc. Note that I have no error handling; make sure to add each value to each time stamp. Btw the timestamp is in milliseconds. Label should be unique.
--- src/PercentageValue.js
+++ src/PercentageValue.js
@@ -3,12 +3,25 @@ import * as React from 'react';
 export default function PercentageValue(props) {
 	const { label, value } = props;
 	const valueToDisplay = Number.parseFloat(value * 100).toPrecision(4);
+	let percentageClassName;
+	if (value > 0.75) {
+		percentageClassName = 'value75';
+	} else if (value > 0.5) {
+		percentageClassName = 'value50';
+	} else if (value > 0.25) {
+		percentageClassName = 'value25';
+	} else {
+		percentageClassName = '';
+	}
 	return <div className={`PercentageValue ${label}`}>
 		<div className="PercentageValue__label">
 			{label}
 		</div>
 		<div className="PercentageValue__value">
 			{valueToDisplay}%
+		</div>
+		<div className="PercentageValue__bar__container">
+			<div className={`PercentageValue__bar ${percentageClassName}`} style={{ width: `${value * 100}%` }}/>
 		</div>
 	</div>
 }
