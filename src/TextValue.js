f376c048a9b09abe7e7572193cb0100d58242aac
Csaba Schreiner <schreiner.csaba@gmail.com>
2018-02-12 22:10:56 +0100

    feat(big-screen): add css and example screen builds, modify templates to include necessary classes

--- src/TextValue.js
+++ src/TextValue.js
@@ -2,11 +2,11 @@ import * as React from 'react';
 
 export default function TextValue(props) {
 	const { label, value, on } = props;
-	return <div className={`TextValue ${label} TextValue--${on ? 'on' :'off' }`}>
-		<div className="TextValue__label">
+	return <div className={`data ${label} data--${on ? 'on' :'off' }`}>
+		<div className="data__label">
 			{label}
 		</div>
-		<div className="TextValue__value">
+		<div className="data__value">
 			{value}
 		</div>
 	</div>
